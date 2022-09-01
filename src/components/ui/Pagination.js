361c5a84b5e1b8efed8ac824c70a0dd2e5d63f3d
juelhossain <juelhossainnn@gmail.com>
2022-09-01 15:41:04 +0600

    pagination modified

--- src/components/ui/Pagination.js
+++ src/components/ui/Pagination.js
@@ -6,6 +6,7 @@ import { fetchVideosCount } from "../../features/videos/videosSlice";
 export default function Pagination() {
   const dispatch = useDispatch();
   const { totalVideos } = useSelector((state) => state.videos);
+  const { page } = useSelector((state) => state.filter);
   const pageCount = parseInt(totalVideos / 3);
   useEffect(() => {
     dispatch(fetchVideosCount());
@@ -13,10 +14,14 @@ export default function Pagination() {
   return (
     <section className="pt-12">
       <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 justify-end">
-        {[...Array(pageCount).keys()].map((num) => (
+        {[...Array(pageCount).keys()].map((num, index) => (
           <button
             key={Math.random()}
-            className="bg-blue-600 text-white px-4 py-1 rounded-full"
+            className={
+              index + 1 === page
+                ? "bg-blue-600 text-white px-4 py-1 rounded-full"
+                : "bg-blue-200 text-blue-500 px-4 py-1 rounded-full"
+            }
             onClick={(e) => {
               dispatch(setPage(parseInt(e.target.innerText)));
             }}
